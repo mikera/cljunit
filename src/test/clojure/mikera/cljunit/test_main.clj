544c9339da4c7bc0b618aecfe4ca20773b6f4e5c
mikera <mike.r.anderson.13@gmail.com>
2013-02-15 19:50:08 +0800

    Switch back to mikera-pom, no need for clojure-maven-plugin

--- src/test/clojure/mikera/cljunit/test_main.clj
+++ src/test/clojure/mikera/cljunit/test_main.clj
@@ -1,10 +1,18 @@
 (ns mikera.cljunit.test-main
   (:use clojure.test)
-  (:use mikera.cljunit.core))
+  (:use mikera.cljunit.core)
+  (:require [bultitude.core :as b]))
 
 (deftest test-core
   (testing "Core"
     (is (= 1 1))))
+
+(deftest test-bultitude
+  (testing "Core"
+    (let [nms (b/namespaces-on-classpath)
+          nmset (into #{} (map str nms))]
+      ;; (println nms)
+      (is (nmset "mikera.cljunit.core")))))
 
 (deftest test-namespaces
   (testing "Core"
